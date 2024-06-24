c45b2961c23290429e1e10671d9282d6a3603e9e
ZennoZenith <ZennoZenith@proton.me>
2024-06-24 15:30:44 +0000

    refa: Restructureing directory

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,3 @@
-use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
-use std::net::TcpListener;
-
-#[derive(Deserialize)]
-struct FormData {
-    name: String,
-    email: String,
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    // .bind("127.0.0.0:8000")?
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
