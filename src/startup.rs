99d6e002e719653dab70f780bce1bebf333c12ac
ZennoZenith <ZennoZenith@proton.me>
2024-06-26 21:31:33 +0000

    feat: Using tracing logger

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,9 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    // `init` does call `set_logger`, so this is all we need to do.
-    // We are falling back to printing all logs at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
