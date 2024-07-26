b0a23e292beca89c5c48d2195fc3de3272ddd432
ZennoZenith <ZennoZenith@proton.me>
2024-07-27 00:13:49 +0530

    feat: Using one time cookie

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -5,19 +5,23 @@ use sqlx::{Pool, Postgres};
 async fn an_error_flash_message_is_set_on_failure(pool: Pool<Postgres>) {
     // Arrange
     let app = spawn_app(pool).await;
-    // Act
+
+    // Act - Part 1 - Try to login
     let login_body = serde_json::json!({
         "username": "random-username",
         "password": "random-password"
     });
     let response = app.post_login(&login_body).await;
-    // Assert
     assert_is_redirect_to(&response, "/login");
 
     let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
     assert_eq!(flash_cookie.value(), "Authentication failed");
 
-    // Act - Part 2
+    // Act - Part 2 - Follow the redirect
     let html_page = app.get_login_html().await;
     assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
+
+    // Act - Part 3 - Reload the login page
+    let html_page = app.get_login_html().await;
+    assert!(!html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
 }
