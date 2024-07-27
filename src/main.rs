7addc5d5db41bc9611aab6d72a1611cb86cda265
ZennoZenith <ZennoZenith@proton.me>
2024-07-27 16:26:44 +0530

    feat: Using actix-session

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = Settings::new().expect("Failed to read configuration.");
-    let application = Application::build(&configuration, None)?;
+    let application = Application::build(&configuration, None).await?;
     application.run_until_stopped().await?;
     Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -3,6 +3,8 @@ use crate::email_client::EmailClient;
 use crate::routes::{
     confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
 };
+use actix_session::storage::RedisSessionStore;
+use actix_session::SessionMiddleware;
 use actix_web::cookie::Key;
 use actix_web::{dev::Server, web, App, HttpServer};
 use actix_web_flash_messages::storage::CookieMessageStore;
@@ -19,10 +21,10 @@ pub struct Application {
 }
 
 impl Application {
-    pub fn build(
+    pub async fn build(
         configuration: &Settings,
         connection_pool: Option<Pool<Postgres>>,
-    ) -> Result<Self, std::io::Error> {
+    ) -> Result<Self, anyhow::Error> {
         let connection_pool = connection_pool.unwrap_or_else(|| {
             PgPoolOptions::new()
                 .acquire_timeout(std::time::Duration::from_secs(2))
@@ -54,7 +56,9 @@ impl Application {
             email_client,
             configuration.application.base_url.clone(),
             HmacSecret(configuration.application.hmac_secret.clone()),
-        )?;
+            configuration.redis_uri.clone(),
+        )
+        .await?;
         Ok(Self { port, server })
     }
 
@@ -75,24 +79,30 @@ impl Application {
 // a raw `String` would expose us to conflicts.
 pub struct ApplicationBaseUrl(pub String);
 
-pub fn run(
+async fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
     hmac_secret: HmacSecret,
-) -> Result<Server, std::io::Error> {
+    redis_uri: Secret<String>,
+) -> Result<Server, anyhow::Error> {
     let db_pool = web::Data::new(db_pool);
     let email_client = web::Data::new(email_client);
     let base_url = web::Data::new(ApplicationBaseUrl(base_url));
 
-    let message_store =
-        CookieMessageStore::builder(Key::from(hmac_secret.0.expose_secret().as_bytes())).build();
+    let secret_key = Key::from(hmac_secret.0.expose_secret().as_bytes());
+    let message_store = CookieMessageStore::builder(secret_key.clone()).build();
     let message_framework = FlashMessagesFramework::builder(message_store).build();
+    let redis_store = RedisSessionStore::new(redis_uri.expose_secret()).await?;
 
     let server = HttpServer::new(move || {
         App::new()
             .wrap(message_framework.clone())
+            .wrap(SessionMiddleware::new(
+                redis_store.clone(),
+                secret_key.clone(),
+            ))
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
