b241e8cd7dd95f344688cfb62abc96242e8e8492
ZennoZenith <ZennoZenith@proton.me>
2024-07-27 19:44:12 +0530

    feat: Change password form

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -6,15 +6,7 @@ use anyhow::Context;
 use sqlx::PgPool;
 use uuid::Uuid;
 
-use crate::session_state::TypedSession;
-
-// Return an opaque 500 while preserving the error's root cause for logging.
-fn e500<T>(e: T) -> actix_web::Error
-where
-    T: std::fmt::Debug + std::fmt::Display + 'static,
-{
-    actix_web::error::ErrorInternalServerError(e)
-}
+use crate::{session_state::TypedSession, utils::e500};
 
 pub async fn admin_dashboard(
     session: TypedSession,
@@ -40,13 +32,18 @@ pub async fn admin_dashboard(
 
             <body>
                 <p>Welcome {username}!</p>
+                <p>Available actions:</p>
+                <ol>
+                <li><a href="/admin/password">Change password</a></li>
+                </ol>
             </body>
 
             </html>"#
         )))
 }
+
 #[tracing::instrument(name = "Get username", skip(pool))]
-async fn get_username(user_id: Uuid, pool: &PgPool) -> Result<String, anyhow::Error> {
+pub async fn get_username(user_id: Uuid, pool: &PgPool) -> Result<String, anyhow::Error> {
     let row = sqlx::query!(
         r#"
         SELECT username
