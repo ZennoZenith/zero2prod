93dcfc843efaebc5990831ee22eb9bad59aa7077
ZennoZenith <ZennoZenith@proton.me>
2024-07-28 01:46:17 +0530

    feat: Logout feature implemented

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -34,8 +34,14 @@ pub async fn admin_dashboard(
                 <p>Welcome {username}!</p>
                 <p>Available actions:</p>
                 <ol>
-                <li><a href="/admin/password">Change password</a></li>
+                    <li><a href="/admin/password">Change password</a></li>
+                    <li>
+                        <form name="logoutForm" action="/admin/logout" method="post">
+                            <input type="submit" value="Logout">
+                        </form>
+                    </li>
                 </ol>
+                
             </body>
 
             </html>"#
